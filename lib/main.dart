453a21b32141b51f109a4e23f7284ea8518f2a5e
HWVictor <victorkindlefire@gmail.com>
2023-01-17 20:43:19 +0400

    fixed a bug

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  runApp(const MyApp());
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
