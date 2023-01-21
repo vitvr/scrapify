ea401364aff2f8aab105696f99cbf3aac84e3d8c
HWVictor <victorkindlefire@gmail.com>
2023-01-21 20:57:23 +0400

    started home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,11 +11,57 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color.fromARGB(255, 255, 139, 111),
+        onPressed: () {},
+      ),
+      appBar: AppBar(
+        title: const Text(
+          'SCRAPIFY',
+          style: TextStyle(fontSize: 35),
+        ),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notification_add_outlined),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Account',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu),
+            label: 'Menu',
+          ),
+        ],
+      ),
+      // backgroundColor: Colors.white,
+      body: const SafeArea(
         child: Center(
           child: Text(
-            'HOME PAGE TODO',
+            'HOME PAGE TODO!',
           ),
         ),
       ),
