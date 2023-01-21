22392be6f0bd967d0d777a81242feb6d73c9790e
HWVictor <victorkindlefire@gmail.com>
2023-01-21 17:16:26 +0400

    did ui for login and register, added hompage file

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scrapify/homepage.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +11,79 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 75),
+                  child: Image(image: AssetImage('assets/mainLogo.png')),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(64, 255, 99, 61),
+                    borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '\t\t\t\t\tUsername',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(64, 255, 99, 61),
+                    borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '\t\t\t\t\tPassword',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 60),
+                  child: Text(
+                    'Forgot your login credentials? Click here.',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color.fromARGB(191, 255, 99, 61),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                    ),
+                    minimumSize: Size(1000000, 50),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:scrapify/onboading2.dart';
+import 'package:scrapify/onboarding2.dart';
 import 'package:scrapify/onboarding.dart';
 import 'firebase_options.dart';
 
