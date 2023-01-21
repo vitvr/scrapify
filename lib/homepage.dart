e76b3c7be78dd5b688fe548d61159c8115374b79
HWVictor <victorkindlefire@gmail.com>
2023-01-21 21:20:41 +0400

    added color to homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,58 +11,68 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color.fromARGB(255, 255, 139, 111),
-        onPressed: () {},
-      ),
-      appBar: AppBar(
-        title: const Text(
-          'SCRAPIFY',
-          style: TextStyle(fontSize: 35),
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(14, 255, 99, 61),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Color.fromARGB(191, 255, 99, 61),
+          child: Icon(Icons.create),
+          onPressed: () {},
         ),
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.notification_add_outlined),
+        appBar: AppBar(
+          // elevation: 0.0,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: Colors.white,
+            // systemNavigationBarDividerColor: Colors.white,
           ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.grey,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+          title: const Text(
+            'SCRAPIFY',
+            style: TextStyle(fontSize: 35),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Map',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_outlined),
-            label: 'Account',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu),
-            label: 'Menu',
-          ),
-        ],
-      ),
-      // backgroundColor: Colors.white,
-      body: const SafeArea(
-        child: Center(
-          child: Text(
-            'HOME PAGE TODO!',
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.notification_add_outlined),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Color.fromARGB(191, 255, 99, 61),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: 'Map',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_outlined),
+              label: 'Account',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.menu),
+              label: 'Menu',
+            ),
+          ],
+        ),
+        // backgroundColor: Colors.white,
+        body: const SafeArea(
+          child: Center(
+            child: Text(
+              'HOME PAGE TODO!',
+            ),
           ),
         ),
       ),
