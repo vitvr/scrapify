a3da88d1b64a0ae6916645bbe56e39e06aec9b2d
francis <sfrancisrussell1@gmail.com>
2023-01-26 10:36:01 +0400

    Added color palette to "colors.dart"

---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import "package:flutter/material.dart";
+
+const dark = Color.fromRGBO(255, 99, 61, 1.0);
+const light = Color.fromRGBO(255, 141, 113, 1.0);
+const lighter = Color.fromRGBO(255, 188, 171, 1.0);
