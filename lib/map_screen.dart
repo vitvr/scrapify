36bdc5dee9e603667867c43ccb6fc7cf47b419f7
francis <sfrancisrussell1@gmail.com>
2023-03-08 02:40:00 +0400

    Added Google Maps to map_screen.dart

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+const LatLng currentLocation = LatLng(25.102288819629432, 55.162227475793415);
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -11,6 +13,12 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+        body: GoogleMap(
+      initialCameraPosition: CameraPosition(
+        target: currentLocation,
+        zoom: 14,
+      ),
+    ));
   }
 }
